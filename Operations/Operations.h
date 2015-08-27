9affca6373e0199bcdc31add7d74404641c28285
Michail Pishchagin <mblsha@yandex-team.ru>
2015-08-27 11:13:10 +0300

    Remove unneeded OS X files.

--- Operations/Operations.h
+++ Operations/Operations.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Daniel Thorpe. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for Operations.
 FOUNDATION_EXPORT double OperationsVersionNumber;
